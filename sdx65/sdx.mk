5d906b79dd9a3814cfb9c3d3e0bc28e713767464
Bjordis Collaku(Temp) <quic_bcollaku@quicinc.com>
2022-04-27 16:16:54 -0700

    owrt-qti-conf: Update sdx65/sdx.mk profile.
    Update sdx.mk profile to remove libexecinfo
    and enable linux-msm-5.4_dt for build in OpenWRT.
    
    Change-Id: I2772be5d8596c11b604b5a5a0df48c8586eeaaf5

--- sdx65/sdx.mk
+++ sdx65/sdx.mk
@@ -4,9 +4,9 @@ UTILS:=file luci-app-samba rng-tools profilerd
 
 COREBSP_UTILS:=pm-utils
 
-QTIBSP:=adbd core-include ext4_utils fs_mgr libbase libcutils liblog libmincrypt mkbootimg libsparse logwrapper usb-composition libexecinfo edk2
+QTIBSP:=adbd core-include ext4_utils fs_mgr libbase libcutils liblog libmincrypt mkbootimg libsparse logwrapper usb-composition edk2
 
-QTIBSPPROP:=common sign_abl ipa_fws
+QTIBSPPROP:=common sign_abl ipa_fws linux-msm-5.4_dt
 
 COREPROP:=diag diag-router qmi-framework rmt_storage tftp-server time-services
 
