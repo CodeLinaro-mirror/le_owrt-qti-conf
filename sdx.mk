38e321b6c14138c52b0d021c8bedc4326122492a
shoudil <quic_shoudil@quicinc.com>
2022-08-30 17:12:35 +0800

    owrt-qti-conf: enable data packages for kuno.
    Change-Id: I16b06d2edf4172a251ee86d4977a21ce459a7493

--- sdx.mk
+++ sdx.mk
@@ -62,6 +62,7 @@ define Profile/SDX35_Kuno
 	PACKAGES:=$(OPENWRT_STANDARD) \
 		$(COREBSP_UTILS) $(UTILS) \
 		$(QTIBSP) $(QTIBSPPROP) $(QTICORE) $(QTICOREPROP) $(QTISSMGR) $(QTISSMGRPROP) \
+		$(QTIDATA) $(QTIDATAPROP) \
 		ipa_fws -linux-msm-5.4_dt -lacpd libtirpc -swconfig
 endef
 
