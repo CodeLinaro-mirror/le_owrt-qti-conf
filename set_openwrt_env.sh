efe05b58a52b6f7f01e57e4d87cefe518ab25da9
Keerthi Gowda Balehalli Satyanarayana <quic_kbalehal@quicinc.com>
2023-06-13 08:51:58 -0700

    qti-conf: Remove prebuilts and copy the kernel dtbs
    Change-Id: Id3568e95e27c4108c6d23cf6db0399dcc3b525d7

--- set_openwrt_env.sh
+++ set_openwrt_env.sh
@@ -36,6 +36,10 @@ TOPDIR=$(pwd)
 
 /bin/cp $TOPDIR/owrt-qti-conf/feeds.conf $TOPDIR
 
+unlink $TOPDIR/src/kernel-5.15/kernel_platform/msm-kernel/arch/arm64/boot/dts/vendor
+mkdir -p $TOPDIR/src/kernel-5.15/kernel_platform/msm-kernel/arch/arm64/boot/dts/vendor
+/bin/cp -rp $TOPDIR/src/kernel-5.15/kernel_platform/qcom/proprietary/devicetree/* $TOPDIR/src/kernel-5.15/kernel_platform/msm-kernel/arch/arm64/boot/dts/vendor/
+
 cd $TOPDIR
 umask 022
 
