35c595bb3f6169ab3c8e5a7b89b04a18363d81e8
shoudil <quic_shoudil@quicinc.com>
2023-08-31 17:36:14 +0800

    owrt-qti-conf: fix the empty issue of recoveryfs image.
    The redesign of kernel config/build will break the separate recovery
    rootfs,which will generate empty recovery rootfs. this change recover
    the logic of separate rootfs.
    
    Change-Id: I5d457f7fc8a06d67a31059b786728f34c0cf0627

--- set_openwrt_env.sh
+++ set_openwrt_env.sh
@@ -289,14 +289,17 @@ function configure(){
 	rm -rf tmp
 	cp owrt-qti-conf/${1}/${2}.config .config || return
 
-	#Create separate rootfs for sdx75 recovery profile
-	if [ "${1}" == "sdx75" ]; then
-		if [ "${2}" == "recovery" ]; then
-			ARCH=$(sed -n -e '/ARCH:/ s/.*= *//p' "target/linux/${1}/Makefile")
-			CPU=$(sed -n -e '/CPU_TYPE:/ s/.*= *//p' "target/linux/${1}/Makefile")
+	#Create separate rootfs for recovery profile
+	if [ "${2}" == "recovery" ]; then
+		ARCH=$(sed -n -e '/ARCH:/ s/.*= *//p' "target/linux/${1}/Makefile")
+		CPU=$(sed -n -e '/CPU_TYPE:/ s/.*= *//p' "target/linux/${1}/Makefile")
+		if [ "${1}" == "sdx35" ]; then
+			CPU_SUBTYPE=$(sed -n -e '/CPU_SUBTYPE:/ s/.*= *//p' "target/linux/${1}/Makefile")
+			BUILD_DIR_CONFIG="CONFIG_TARGET_ROOTFS_DIR="\"$TOPDIR"/build_dir/target-"${ARCH}"_"${CPU}"+"${CPU_SUBTYPE}"_musl_eabi/recovery"\"
+		else
 			BUILD_DIR_CONFIG="CONFIG_TARGET_ROOTFS_DIR="\"$TOPDIR"/build_dir/target-"${ARCH}"_"${CPU}"_musl/recovery"\"
-			sed -i '$a'"$BUILD_DIR_CONFIG"'' .config
 		fi
+		sed -i '$a'"$BUILD_DIR_CONFIG"'' .config
 	fi
 
 	make defconfig
