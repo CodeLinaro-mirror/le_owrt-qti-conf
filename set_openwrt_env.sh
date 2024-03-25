3c8e652f27ecce667e00bfbf59c161cb5b049da3
QU TAO <quic_taoq@quicinc.com>
2024-03-25 10:57:01 +0530

    Revert "owrt-qti-conf: gplv2"
    This reverts commit 860e6a95ffd4f87b48b49881fa175c34507f780f.
    
    Reason for revert: <no need for external build>
    
    Change-Id: I18643ee62a462983c86c2a5540f1d817ccef52ca

--- set_openwrt_env.sh
+++ set_openwrt_env.sh
@@ -83,11 +83,6 @@ function set_up_feeds(){
 	rm -rf feeds
 	./scripts/feeds update -a || return
 	./scripts/feeds install -a || return
-	if [ "${1}" == "sdx75" ] && [ "${OWRT_VERSION%%.*}" == "22" ]; then
-		./scripts/feeds uninstall bash || return
-		./scripts/feeds uninstall xz || return
-		./scripts/feeds install -a -f -p qtigplv2 || return
-	fi
 }
 
 function patch_upstream_feeds(){
@@ -349,7 +344,7 @@ function configure(){
 		sed -i 's/OWRT_VERSION:=.*/OWRT_VERSION:='$OWRT_VERSION'/g' target/linux/${1}/Makefile;
 	fi
 
-	set_up_feeds ${1} || return 1
+	set_up_feeds || return 1
 	patch_upstream_feeds || return 1
 	patch_openssl ${1} || return 1
 	rm -rf .config
