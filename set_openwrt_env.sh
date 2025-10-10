ecab894fcda218c8b24dbfe015482fd4d1f3d54f
Bijeesh B <bijeb@qti.qualcomm.com>
2025-10-09 23:49:40 -0700

    Revert "sdx85: Add new kernel defconfig for sdx85-Prpl builds"
    This reverts commit 482a451924dceb4280c2088d289bc134a6d92a2b.
    
    Reason for revert: will merge again after data changes are available
    
    Change-Id: I468308bfe50d054d195db121ec55404e5c7657f0

--- set_openwrt_env.sh
+++ set_openwrt_env.sh
@@ -472,7 +472,7 @@ function configure(){
 			TARGET=sdxpinn-prpl
 		fi
 		if [ "${1}" == "sdx85" ]; then
-			TARGET=sdxkova.prpl
+			TARGET=sdxkova.cpe.wkk
 		fi
 	else
 		if [ "${1}" == "sdx75" ]; then
