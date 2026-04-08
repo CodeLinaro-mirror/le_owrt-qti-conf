0c4e82d74eba862be6b2a8098cf3b8f7197a419d
Xueqian Nie <xueqnie@qti.qualcomm.com>
2026-04-08 15:01:55 +0800

    add build command in build.py and set_openwrt_env.sh for nbntn
    Change-Id: I80c1c7cf1dd46117547d00aa9044f76b7187b4ea

--- set_openwrt_env.sh
+++ set_openwrt_env.sh
@@ -487,6 +487,10 @@ function configure(){
 		BUILD_WITH_MEMOPT=0
 		if [ "${2}" == "mbb" || "${2}" == "iot" ]; then
 			TARGET=sdxbaagha
+		elif [ "${2}" == "iot" ]; then
+			TARGET=sdxbaagha-iot
+		elif [ "${2}" == "mbb-nbntn" ]; then
+			TARGET=sdxbaagha-nbntn
 		elif [ "${2}" == "mbb-128m" ] || [ "${2}" == "m2-128m" ]; then
 			BUILD_WITH_MEMOPT=1
 			TARGET=sdxbaagha-128m
